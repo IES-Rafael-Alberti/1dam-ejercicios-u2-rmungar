cf02a182e549348ae801e07b14700c811e0fc461
rmungar1209 <rmungar1209@g.educaand.es>
2023-10-27 00:52:24 +0200

    27/10

--- src/Ejercicio2.py
+++ src/Ejercicio2.py
@@ -1,10 +1,20 @@
-contraseña_guardada="rmumngar1209"
-
-def contraseña(contraseña_introducida=(input("Escriba la contraseña: "))):
-    contraseña_introducida=contraseña_introducida.lower()
+contraseña_guardada ="rmungar1209"
+contraseña_introducida = (input("Escriba la contraseña: "))
+def contraseña(contraseña_introducida):
+    contraseña_introducida = contraseña_introducida.lower()
     if contraseña_introducida==contraseña_guardada:
         return "Contraseña correcta"
     else:
         return "Contraseña incorrecta"
-    
+def main():
+    contraseña_introducida = contraseña()
+
+    if contraseña(contraseña_introducida) == contraseña_guardada :
+        print("Contraseña correcta.")
+    else:
+        print("Contraseña incorrecta.")
+
+
+if __name__ == "__main__":
+    main()    
     
