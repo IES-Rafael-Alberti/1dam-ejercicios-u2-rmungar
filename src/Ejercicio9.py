0d756d788f9c3cafb6d6af4d4e940a7d8a83ea35
rmungar <mgraul@iesrafaelalberti.es>
2023-10-30 20:31:35 +0100

    lsego

--- src/Ejercicio9.py
+++ src/Ejercicio9.py
@@ -2,7 +2,7 @@ def calcular_precio_entrada(edad):
     if edad < 4:
         return "La entrada es gratis"  
     elif 4 <= edad <= 18:
-        return "La entradaa cuesta 5€"  
+        return "La entrada cuesta 5€"  
     else:
         return "La entrada cuesta 10€" 
 
---
+++ tests/test_Ejercicio9.py
@@ -0,0 +1,15 @@
+import pytest
+from src.Ejercicio9 import calcular_precio_entrada
+
+@pytest.mark.parametrize(
+    "edad, expected",
+    [
+        (1, "La entrada es gratis"),
+        (18, "La entrada cuesta 5€"),
+        (100, "La entrada cuesta 10€"),
+        (5, "La entrada cuesta 5€"),
+        (17, "La entrada cuesta 5€"),
+        (125, "La entrada cuesta 10€")
+])
+def test_entrada_params(edad, expected):
+    assert calcular_precio_entrada(edad) == expected
