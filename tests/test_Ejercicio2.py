da9425aa0e1e0a5be51e72e68477e5c64beba893
rmungar <mgraul@iesrafaelalberti.es>
2023-10-31 17:36:47 +0100

    as

---
+++ tests/test_Ejercicio2.py
@@ -0,0 +1,14 @@
+import pytest
+from src.P2_1.Ejercicio2 import contraseña
+
+@pytest.mark.parametrize(
+"contraseña_introducida, expected",
+    [
+      ("rmungar1209", "Contraseña correcta"),
+      ("RMUNGAR1209", "Contraseña correcta"),
+      ("contraseña", "Contraseña incorrecta"),
+      ("aitortilla", "Contraseña incorrecta"),
+      ("noaitortilla", "Contraseña incorrecta")
+    ])
+def test_contraseña_params(contraseña_introducida, expected):
+    assert contraseña(contraseña_introducida) == expected
