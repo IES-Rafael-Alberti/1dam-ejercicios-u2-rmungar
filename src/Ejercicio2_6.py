7471939e21062664c2b18e6a32724b1d9be87543
rmungar1209 <rmungar1209@g.educaand.es>
2023-11-04 13:53:30 +0100

    a

--- src/Ejercicio2_6.py
+++ src/Ejercicio2_6.py
@@ -1,8 +1,10 @@
 def piramide(num):
-    cont = 1
-    while cont <= num:
-        print("* " * cont)
+    cont = 0
+    while cont < num:
         cont += 1
+        print ("* "*cont)
+    
+        
         
 
 def main():
--- src/Ejercicio2_8.py
+++ src/Ejercicio2_8.py
@@ -1,2 +1,15 @@
-def triangulo():
-    return 1
+def triangulo(num):
+    for i in range(1, num+1, 2):
+        for a in range(i, 0, -2):
+            print(a, end=" ")
+        print("")
+
+
+def main():
+    num = int(input("Introduzca la altura del triángulo: "))
+    print(triangulo(num))
+ 
+
+
+if __name__ == "__main__":
+    main()    
